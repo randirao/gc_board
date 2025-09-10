00d41d32284b7e5b1724d2257ccf12a22985b235
randirao <randirao4245@gmail.com>
2025-09-10 23:57:37 +0900

    PageLimitCalculator 테스트

---
+++ service/article/src/test/java/gc/board/article/service/PageLimitCalculatorTest.java
@@ -0,0 +1,22 @@
+package gc.board.article.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PageLimitCalculatorTest {
+
+    @Test
+    public void calculatePageLimitTest() {
+        calculatePageLimitTest(1L, 30L, 10L, 301L);
+        calculatePageLimitTest(7L, 30L, 10L, 301L);
+        calculatePageLimitTest(10L, 30L, 10L, 301L);
+        calculatePageLimitTest(11L, 30L, 10L, 601L);
+        calculatePageLimitTest(12L, 30L, 10L, 601L);
+    }
+
+    private void calculatePageLimitTest(Long page, Long pageSize, Long movablePageCount, Long expected) {
+        Long actual = PageLimitCalculator.calculatePageLimit(page, pageSize, movablePageCount);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
